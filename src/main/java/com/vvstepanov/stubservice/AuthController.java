07554a52b9758d74d2ffeaad00c59793e5f2d279
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2026-01-18 20:34:06 +0300

    Добавлены задержки в 2 секунды

--- src/main/java/com/vvstepanov/stubservice/AuthController.java
+++ src/main/java/com/vvstepanov/stubservice/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api")
@@ -14,11 +15,23 @@ public class AuthController {
 
     @GetMapping("/user")
     public UserResponse getUser() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
         return new UserResponse("Login1", "ok");
     }
 
     @PostMapping("/user")
     public CreateUserResponse createUser(@RequestBody UserRequest userRequest) {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
         String currentDate = LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
